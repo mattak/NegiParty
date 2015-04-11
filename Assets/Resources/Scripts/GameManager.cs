a644b0ec812f3236bb9f12d1cd42aadfd616a740
Takuma Maruyama <maruyama@mfac.jp>
2015-04-11 17:15:37 +0900

    master feat(Scene): Create MainScene & StartGame

---
+++ Assets/Resources/Scripts/GameManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : SingletonMonoBehaviourFast<GameManager> {
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	public void GameStart() {
+		Application.LoadLevel ("Main");
+	}
+}
---
+++ Assets/Resources/Scripts/SceneController/StartGameSceneController.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartGameSceneController : MonoBehaviour {
+	public void OnClickStartButton() {
+		Debug.Log ("onClick");
+		GameManager.Instance.GameStart ();
+	}
+}
